49c57db49310075112ac6c9054e56500cb279132
Artur Mushulov <bigmanar2r@gmail.com>
2022-09-01 10:59:42 +0300

    Router V6

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,42 +1,22 @@
-import { useState } from "react";
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
-import ErrorBoundaries from "../errorBoundaries/ErrorBoundaries";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage } from "../pages";
+import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
-    
-    const [selectedChar, setChar] = useState(null);
-
-    const onCharSelected = (id) => {
-        setChar(id);
-    }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-            {/* <ErrorBoundaries>
-                <RandomChar/>
-            </ErrorBoundaries>
-                <div className="char__content">
-                    <ErrorBoundaries>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundaries>
-                    <ErrorBoundaries>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundaries>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<MainPage/>}/>                  
+                        <Route path="/comics" element={<ComicsPage/>}/>                  
+                    </Routes>
+                </main>
+            </div>
+        </Router>
     )
 }
 
